4192c23a49fd10ab9ddadcf08ed15793082a76ce
Fchen <mario@fridrichovsky.at>
2018-07-22 13:17:07 +0200

    defined constant vars to constant vars

--- utils/commandHandler.js
+++ utils/commandHandler.js
@@ -4,10 +4,10 @@ module.exports.run = (Client, msg) => {
 
         if(msg.isMemberMentioned(msg.guild.me)) return Client.functions.createEmbed(msg.channel, texts.general_mentioned_text + " **`" + Client.prefix + "`**", texts.general_mentioned_title);
 
-        var invoke = msg.content.split(' ')[0].substr(Client.prefix.length);
-        var args   = msg.content.split(' ').slice(1);
+        const invoke = msg.content.split(' ')[0].substr(Client.prefix.length);
+        const args   = msg.content.split(' ').slice(1);
     
-        var cmd = Client.commands.get(invoke);
+        const cmd = Client.commands.get(invoke);
 
         if(cmd) {
             cmd.run(Client, msg, args, true);
